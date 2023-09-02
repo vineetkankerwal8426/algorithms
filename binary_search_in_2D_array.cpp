410c02c8695426d6260bd61c1b20cc6ee85af006
Vineet Kumar Kankerwal <codevkankerwal@gmail.com>
2023-09-02 22:41:39 +0530

    algorithm for searching in 2d array with O(n) time complexity

---
+++ binary_search_in_2D_array.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// for the array that is not strictly sorted. Example :
+// {
+// {10,20,30,40},
+// {15,25,35,45},
+// {28,29,37,49},
+// {33,34,38,50}
+// }
+
+vector<int> search(vector<vector<int>> &arr, int target)
+{
+    int row = 0;
+    int col = arr[0].size() - 1;
+    vector<int> ans = {-1, -1};
+    while (row < arr.size() && col >= 0)
+    {
+        if (arr[row][col] == target)
+        {
+            ans[0] = row;
+            ans[1] = col;
+            return {row, col};
+        }
+        else if (arr[row][col] < target)
+        {
+            row++;
+        }
+        else
+        {
+            col--;
+        }
+    }
+    return {-1, -1};
+}
