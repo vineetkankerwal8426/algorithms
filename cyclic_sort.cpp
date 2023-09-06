d17faa3bcd4ed177765d4b34dd9e28e9a638dce5
Vineet Kumar Kankerwal <codevkankerwal@gmail.com>
2023-09-06 12:46:23 +0530

    cyclic sort algorithm commited

---
+++ cyclic_sort.cpp
@@ -0,0 +1,19 @@
+// use when array have value in range(m,n) 
+// in this case array have value in range(1,n) {5,4,3,2,1} after sort - {1,2,3,4,5} range is in 1 to 5. Time complexity is O(n)
+
+
+#include<iostream>
+using namespace std;
+
+void cyclicSort(int arr[],int size){
+    int i =0;
+    while(i<size){
+        int correct = arr[i] -1;
+        if(arr[i]!= arr[correct]){
+            swap(arr[i],arr[correct]);
+        }
+        else{
+            i++;
+        }
+    }
+}
