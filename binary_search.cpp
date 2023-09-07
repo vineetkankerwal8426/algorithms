8d607a2472f1906e5e47ef8023df6fbfadefcb1e
Vineet Kumar Kankerwal <codevkankerwal@gmail.com>
2023-09-07 21:37:55 +0530

    recursive method for binary search added

--- binary_search.cpp
+++ binary_search.cpp
@@ -32,3 +32,20 @@ int binary(int arr[],int target,int size){
     return -1;
 }
 
+
+
+//RECURSIVE METHOD
+
+int binary(int arr[], int start, int end, int target)
+{   
+    if (start > end)
+        return -1;
+    int mid = start + (end - start) / 2;
+    if (arr[mid] == target)
+        return mid;
+    else if (arr[mid] < target)
+        return binary(arr, mid + 1, end, target);
+    else
+        return binary(arr, start, mid - 1, target);
+}
+
