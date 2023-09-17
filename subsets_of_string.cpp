0c4e984c8b457df2b41ccde72f89649842221a5a
Vineet Kumar Kankerwal <codevkankerwal@gmail.com>
2023-09-17 16:40:53 +0530

    algo to find all subsets of a string

---
+++ subsets_of_string.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+// if you want to return ans in array
+
+vector<string> subseq(string p, string up){  // p = processed string : initially empty,,,,,,, up = unprocessed string : string from which you want all subsets;
+    if(up.empty()){
+        vector<string> ans = {p};
+        return ans;
+    }
+    char ch = up.front();
+    
+    vector<string>left = subseq(p+ch,up.substr(1));
+    vector<string>right = subseq(p,up.substr(1));
+    left.insert(left.end(),right.begin(),right.end());
+    return left;
+}
+
+// if you only print all subsets of a string 
+
+void subseq(string p, string up){
+    if (up.empty()){
+        cout<<p;
+        return;
+    }
+    char ch = up.front();
+
+    subseq(p+ch,up.substr(1));
+    subseq(p,up.substr(1));
+}
